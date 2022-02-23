faec50dc8d0d507e762e49c4e2ace92d307ac97d
石翔宇 <xyu.shi@outlook.com>
2022-02-23 18:28:06 +0800

    rename script

--- CAlgs/scripts/run_other.sh
+++ CAlgs/scripts/run_other.sh
@@ -1,4 +1,4 @@
- #
+
 CUDA_VISIBLE_DEVICES=7 nohup python scripts/run.py cifar100 vgg13 1 0.6 > ./logs/log_run_cifar100_vgg13_1_0.6.log &
 CUDA_VISIBLE_DEVICES=7 nohup python scripts/run.py cifar100 vgg13 3 0.6 > ./logs/log_run_cifar100_vgg13_3_0.6.log &
 CUDA_VISIBLE_DEVICES=7 nohup python scripts/run.py cifar100 vgg13 4 0.6 > ./logs/log_run_cifar100_vgg13_4_0.6.log &
@@ -6,7 +6,6 @@ CUDA_VISIBLE_DEVICES=7 nohup python scripts/run.py cifar100 vgg13 5 0.6 > ./logs
 CUDA_VISIBLE_DEVICES=6 nohup python scripts/run.py cifar100 vgg13 7 0.6 > ./logs/log_run_cifar100_vgg13_7_0.6.log &
 CUDA_VISIBLE_DEVICES=6 nohup python scripts/run.py cifar100 vgg13 2 0.6 > ./logs/log_run_cifar100_vgg13_2_0.6.log &
  
-#
 CUDA_VISIBLE_DEVICES=6 nohup python scripts/run.py cifar100 vgg13 1 0.3 > ./logs/log_run_cifar100_vgg13_1_0.3.log &
 CUDA_VISIBLE_DEVICES=6 nohup python scripts/run.py cifar100 vgg13 3 0.3 > ./logs/log_run_cifar100_vgg13_3_0.3.log &
 CUDA_VISIBLE_DEVICES=5 nohup python scripts/run.py cifar100 vgg13 4 0.3 > ./logs/log_run_cifar100_vgg13_4_0.3.log &
@@ -14,7 +13,6 @@ CUDA_VISIBLE_DEVICES=5 nohup python scripts/run.py cifar100 vgg13 5 0.3 > ./logs
 CUDA_VISIBLE_DEVICES=5 nohup python scripts/run.py cifar100 vgg13 7 0.3 > ./logs/log_run_cifar100_vgg13_7_0.3.log &
 CUDA_VISIBLE_DEVICES=5 nohup python scripts/run.py cifar100 vgg13 2 0.3 > ./logs/log_run_cifar100_vgg13_2_0.3.log &
 
- #
 CUDA_VISIBLE_DEVICES=4 nohup python scripts/run.py cifar100 vgg19 1 0.6 > ./logs/log_run_cifar100_vgg19_1_0.6.log &
 CUDA_VISIBLE_DEVICES=4 nohup python scripts/run.py cifar100 vgg19 3 0.6 > ./logs/log_run_cifar100_vgg19_3_0.6.log &
 CUDA_VISIBLE_DEVICES=4 nohup python scripts/run.py cifar100 vgg19 4 0.6 > ./logs/log_run_cifar100_vgg19_4_0.6.log &
@@ -22,7 +20,6 @@ CUDA_VISIBLE_DEVICES=4 nohup python scripts/run.py cifar100 vgg19 5 0.6 > ./logs
 CUDA_VISIBLE_DEVICES=3 nohup python scripts/run.py cifar100 vgg19 7 0.6 > ./logs/log_run_cifar100_vgg19_7_0.6.log &
 CUDA_VISIBLE_DEVICES=3 nohup python scripts/run.py cifar100 vgg19 2 0.6 > ./logs/log_run_cifar100_vgg19_2_0.6.log &
  
-#
 CUDA_VISIBLE_DEVICES=3 nohup python scripts/run.py cifar100 vgg19 1 0.3 > ./logs/log_run_cifar100_vgg19_1_0.3.log &
 CUDA_VISIBLE_DEVICES=3 nohup python scripts/run.py cifar100 vgg19 3 0.3 > ./logs/log_run_cifar100_vgg19_3_0.3.log &
 CUDA_VISIBLE_DEVICES=2 nohup python scripts/run.py cifar100 vgg19 4 0.3 > ./logs/log_run_cifar100_vgg19_4_0.3.log &
@@ -30,7 +27,6 @@ CUDA_VISIBLE_DEVICES=2 nohup python scripts/run.py cifar100 vgg19 5 0.3 > ./logs
 CUDA_VISIBLE_DEVICES=2 nohup python scripts/run.py cifar100 vgg19 7 0.3 > ./logs/log_run_cifar100_vgg19_7_0.3.log &
 CUDA_VISIBLE_DEVICES=2 nohup python scripts/run.py cifar100 vgg19 2 0.3 > ./logs/log_run_cifar100_vgg19_2_0.3.log &
 
-#
 CUDA_VISIBLE_DEVICES=7 nohup python scripts/run.py cifar10 resnet20 1 0.6 > ./logs/log_run_cifar10_resnet20_1_0.6.log &
 CUDA_VISIBLE_DEVICES=7 nohup python scripts/run.py cifar10 resnet20 3 0.6 > ./logs/log_run_cifar10_resnet20_3_0.6.log &
 CUDA_VISIBLE_DEVICES=6 nohup python scripts/run.py cifar10 resnet20 4 0.6 > ./logs/log_run_cifar10_resnet20_4_0.6.log &
@@ -38,7 +34,6 @@ CUDA_VISIBLE_DEVICES=6 nohup python scripts/run.py cifar10 resnet20 5 0.6 > ./lo
 CUDA_VISIBLE_DEVICES=5 nohup python scripts/run.py cifar10 resnet20 7 0.6 > ./logs/log_run_cifar10_resnet20_7_0.6.log &
 CUDA_VISIBLE_DEVICES=5 nohup python scripts/run.py cifar10 resnet20 2 0.6 > ./logs/log_run_cifar10_resnet20_2_0.6.log &
 
-#
 CUDA_VISIBLE_DEVICES=4 nohup python scripts/run.py cifar10 resnet20 1 0.3 > ./logs/log_run_cifar10_resnet20_1_0.3.log &
 CUDA_VISIBLE_DEVICES=4 nohup python scripts/run.py cifar10 resnet20 3 0.3 > ./logs/log_run_cifar10_resnet20_3_0.3.log &
 CUDA_VISIBLE_DEVICES=3 nohup python scripts/run.py cifar10 resnet20 4 0.3 > ./logs/log_run_cifar10_resnet20_4_0.3.log &
@@ -46,15 +41,13 @@ CUDA_VISIBLE_DEVICES=3 nohup python scripts/run.py cifar10 resnet20 5 0.3 > ./lo
 CUDA_VISIBLE_DEVICES=2 nohup python scripts/run.py cifar10 resnet20 7 0.3 > ./logs/log_run_cifar10_resnet20_7_0.3.log &
 CUDA_VISIBLE_DEVICES=2 nohup python scripts/run.py cifar10 resnet20 2 0.3 > ./logs/log_run_cifar10_resnet20_2_0.3.log &
 
-#
 CUDA_VISIBLE_DEVICES=2 nohup python scripts/run.py cifar10 resnet164 1 0.6 > ./logs/log_run_cifar10_resnet164_1_0.6.log &
 CUDA_VISIBLE_DEVICES=3 nohup python scripts/run.py cifar10 resnet164 3 0.6 > ./logs/log_run_cifar10_resnet164_3_0.6.log &
 CUDA_VISIBLE_DEVICES=4 nohup python scripts/run.py cifar10 resnet164 4 0.6 > ./logs/log_run_cifar10_resnet164_4_0.6.log &
 CUDA_VISIBLE_DEVICES=5 nohup python scripts/run.py cifar10 resnet164 5 0.6 > ./logs/log_run_cifar10_resnet164_5_0.6.log &
 CUDA_VISIBLE_DEVICES=6 nohup python scripts/run.py cifar10 resnet164 7 0.6 > ./logs/log_run_cifar10_resnet164_7_0.6.log &
 CUDA_VISIBLE_DEVICES=7 nohup python scripts/run.py cifar10 resnet164 2 0.6 > ./logs/log_run_cifar10_resnet164_2_0.6.log &
-
-# 
+ 
 CUDA_VISIBLE_DEVICES=5 nohup python scripts/run.py cifar10 resnet164 1 0.3 > ./logs/log_run_cifar10_resnet164_1_0.3.log &
 CUDA_VISIBLE_DEVICES=6 nohup python scripts/run.py cifar10 resnet164 3 0.3 > ./logs/log_run_cifar10_resnet164_3_0.3.log &
 CUDA_VISIBLE_DEVICES=7 nohup python scripts/run.py cifar10 resnet164 4 0.3 > ./logs/log_run_cifar10_resnet164_4_0.3.log &
